1032ace02ab007a5d4062c2c1aaaef7239b7f5bb
Ruidozo <128640010+Ruidozo@users.noreply.github.com>
2025-06-13 07:11:20 +0100

    organização dos scripts e preparação para produção

--- airflow-GOOGLE-NEWS-SCRAPER/scripts/google_scraper/tests/test_google_news_access.py
+++ airflow-GOOGLE-NEWS-SCRAPER/scripts/google_scraper/tests/test_google_news_access.py
@@ -1,3 +1,7 @@
+#TODO: Comentarios de código em português e preparado para produção
+# File: airflow-GOOGLE-NEWS-SCRAPER/scripts/google_scraper/tests/test_google_news_access.py
+# Script para testar o acesso ao RSS do Google News e verificar se o serviço está a funcionar sem bloqueios
+
 import requests
 import feedparser
 import time
